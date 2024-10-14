d79f12997ab9e38a2769d7d0787b560337adbbef
Dobauron <safetysafetystrazak@gmail.com>
2024-10-14 10:52:39 +0200

    update Event model

--- event/models.py
+++ event/models.py
@@ -27,17 +27,13 @@ class Event(models.Model):
         null=True,
         help_text="Załącznik wydarzenia",
     )
-    links = models.URLField(blank=True, null=True, help_text="Powiązane linki URL")
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, help_text="Cena wydarzenia"
-    )
-    notes = models.TextField(
-        blank=True, null=True, help_text="Notatki dotyczące wydarzenia"
-    )
+    links = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(blank=True, null=True)
 
     # Typ wydarzenia
     event_type = models.CharField(max_length=50, choices=EVENT_TYPE_CHOICES)
-    event_sub_type = models.CharField(max_length=50, default="default")
+    event_sub_type = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.name} ({self.event_type})"
