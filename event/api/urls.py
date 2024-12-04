63d38d9fd91fc8a819066c0870cce9f937341289
Dobauron <safetysafetystrazak@gmail.com>
2024-12-04 23:22:31 +0100

    move router.urls to end of urlpatterns in event

--- event/api/urls.py
+++ event/api/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r"", EventViewSet)
 
 urlpatterns = [
-    path("/", include(router.urls)),
     path("sub-types/", EventSubLabelAPIView.as_view(), name="event-subtypes"),
     path("types/", EventLabelAPIView.as_view(), name="event-label"),
+    path("", include(router.urls)),
 ]
